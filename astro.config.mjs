b464e4e506ed378f704964620959591553625680
Gonza <gonzalogramagia@gmail.com>
2025-10-05 00:26:22 -0300

    Adapter Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
   site: 'https://ajedrezfamaf.com',
   output: 'server',
+  adapter: vercel(),
   markdown: {
     shikiConfig: {
       theme: 'nord',
