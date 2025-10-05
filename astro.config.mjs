eca6304b1ebf40d25fdfc9b5bf0a496b6b85ba4f
Gonza <gonzalogramagia@gmail.com>
2025-10-05 00:35:04 -0300

    Build funcional

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
-import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
   site: 'https://ajedrezfamaf.com',
-  output: 'hybrid',
-  adapter: vercel(),
+  output: 'static',
   markdown: {
     shikiConfig: {
       theme: 'nord',
