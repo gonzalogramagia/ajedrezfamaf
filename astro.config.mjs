15ce27835326a496aaf21c8917f57e611b05b3a4
Gonza <gonzalogramagia@gmail.com>
2025-10-05 00:42:59 -0300

    Adapter Vercel con rutas API

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
   site: 'https://ajedrezfamaf.com',
   output: 'hybrid',
+  adapter: vercel(),
   markdown: {
     shikiConfig: {
       theme: 'nord',
